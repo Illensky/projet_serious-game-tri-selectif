322e418316ce79757968c59ee4f177ec361b5442
Illensky <alexis.laroche.02240@gmail.com>
2021-12-08 14:08:59 +0100

    fix waste drag display bug

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -30,6 +30,7 @@ const buckets = document.querySelectorAll(".bucket");
 
 
 // define function to display a new waste and remove the actual one from the array
+
 function newWaste () {
     const random = Math.floor(Math.random() * wastes.length);
     actualWaste = wastes[random]
@@ -38,32 +39,28 @@ function newWaste () {
     newWaste.classList.add("waste");
     newWaste.alt = wastes[random][1];
     newWaste.id = wastes[random][0]
+    newWaste.addEventListener('dragstart', dragStart);
+    newWaste.addEventListener('dragend', dragEnd);
     wastes.splice(random, 1)
     document.querySelector("#wasteContainer").appendChild(newWaste);
 }
 
 newWaste();
-let waste = document.querySelector(".waste");
+
 
 // define function to drag the waste
 
 function dragStart() {
-    waste = document.querySelector(".waste");
-    setTimeout(() => (this.classList = 'invisible'), 0);
+
+    setTimeout(() => this.classList = 'invisible', 0);
 }
 
 function dragEnd() {
     this.classList = 'waste';
 }
 
-// asign dragging function to the waste
-
-waste.addEventListener('dragstart', dragStart);
-waste.addEventListener('dragend', dragEnd);
-
 
 // define functions to drop the waste on the buckets
-
 
 function dragOver(e) {
     e.preventDefault()
@@ -78,11 +75,10 @@ function dragLeave() {
     this.classList = 'bucket';
 }
 
-
 function dragDrop(e) {
     e.preventDefault()
     this.classList = 'bucket';
-    waste = document.querySelector("img")
+    const waste = document.querySelector("img");
     if (waste.id === this.id) {
         score++;
     }
@@ -100,6 +96,7 @@ function dragDrop(e) {
 }
 
 // asign dropping functions to all buckets
+
 
 for (const bucket of buckets) {
     bucket.addEventListener('dragover', dragOver);
